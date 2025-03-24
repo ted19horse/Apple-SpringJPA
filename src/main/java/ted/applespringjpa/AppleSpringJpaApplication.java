57bf4fc5de37d1276cc29ac6494793cd4b422858
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-03-25 00:49:31 +0900

    init

---
+++ src/main/java/ted/applespringjpa/AppleSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package ted.applespringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleSpringJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AppleSpringJpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ted/applespringjpa/AppleSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ted.applespringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleSpringJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
